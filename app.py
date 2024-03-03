7f3569ccd644ec7fcad81546e7f55a766809ef4e
Felix Revert <felix@felixs-mbp.home>
2024-03-03 15:15:45 +0100

    3rd commit

--- app.py
+++ app.py
@@ -1,64 +1,156 @@
-# app.py
 
 from flask import Flask, render_template, request
 import os
 import requests
 import json
+
+# Mistal AI & Open AI
 from mistralai.client import MistralClient
 from mistralai.models.chat_completion import ChatMessage
 from openai import OpenAI
+
+# AWS Dynamodb
+import boto3
+
 from dotenv import load_dotenv
+load_dotenv()
+
+language='fr'
 
 app = Flask(__name__, template_folder='.')
-load_dotenv()
+
 client_mistalai = MistralClient(api_key=os.environ["MISTRALAI_API_KEY"])
 client_openai = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
-messages = [ChatMessage(role="user", content="What is the best French cheese?")]
-model="open-mistral-7b"
+
+region_aws='eu-west-3'
+table_name = 'definitions'
+client_dynamodb = boto3.client('dynamodb',aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"], 
+    aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"], region_name=region_aws)
 
 # Home route
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# Route for handling form submission
+# Submission route
 @app.route('/', methods=['POST'])
 def define_word():
     word = request.form['word'].lower()
-    definition=generate_definition(word)
-    return render_template('index.html', definition=definition, image=generate_image(word, definition))
+    if len(word)<2:
+        return render_template('index.html')
 
-# Function to generate definition for the word
+    if not word_exists(word, language):
+        definition = generate_definition(word)
+        prompt_image = generate_prompt_image_generation(word, definition)
+        image_url = generate_image(prompt_image)
+        caption = generate_image_caption(prompt_image, word, definition)
+        add_item_to_db(word, definition, language, image_url, caption)
+
+    else:
+        item = get_word(word, language)['Items'][0]
+        definition = item['definition']['S']
+        image_url = item['image_url']['S']
+        caption = item['caption']['S']
+
+    return render_template('index.html', validite="Le mot "+word+" est valide au scrabble.", 
+        definition=definition, image=image_url, image_caption=caption)
+
+### GenAi functions
 def generate_definition(word):
     chat_response = client_mistalai.chat(
-        model=model,
+        model="open-mistral-7b",
         messages=[ChatMessage(role="user", content=prompt_definition(word))],
     )
     return chat_response.choices[0].message.content
 
 def prompt_definition(word):
     return '''Tu es un académicien français qui donne des définitions à des mots français en français. 
-    Fais comme si le mot "+word+" existait. Maintenant, définis ce mot en utilisant la syntaxe suivante : 
+    Fais comme si le mot '''+word+''' existait. Maintenant, définis ce mot en utilisant la syntaxe suivante : 
     <MOT> : (<TYPE>) (<ORIGINE>) <DEFINITION>. <MOT> est '''+word+''', <TYPE> est soit un nom, 
     verbe, ajectif, adverbe, ou pronom, <ORIGINE> est une courte phrase qui donne l'origine du mot, par exemple "provient du latin", 
     mais peut aussi être breton, basque, occitan par exemple, et <DEFINITION> est la définition que tu donnes à ce mot.
     La syntaxe est donc <MOT> : (<TYPE>) (<ORIGINE>) <DEFINITION>. A noter que <TYPE>, <ORIGINE> et <DEFINITION> 
-    doivent être cohérents entre eux.'''
+    doivent être cohérents entre eux. Retire toutes les balises < et > de la définition.'''
 
-def generate_image(word, definition):
+def generate_image(prompt_image):
     response = client_openai.images.generate(
         model="dall-e-3",
-        prompt=prompt_image(word, definition),
+        prompt=prompt_image,
         size="1024x1024",
         quality="standard",
         n=1,
     )
     image_url = response.data[0].url
-    print(image_url)
     return image_url
 
-def prompt_image(word, definition):
-    return "Génère une image pour ce mot : "+word+" qui a la définition suivante : "+definition
+def generate_prompt_image_generation(word, definition):
+    chat_response = client_mistalai.chat(
+        model="open-mistral-7b",
+        messages=[ChatMessage(role="user", content='''Emploie le terme : '''+word+''' qui a la définition 
+            suivante : '''+definition+''' dans une phrase. La phrase doit être plausible, comme sortie 
+            d'un ouvrage ou d'un journal, en respectant au mieux la définition du mot.''')]
+    )
+    return chat_response.choices[0].message.content
+
+def generate_image_caption(prompt_image, word, definition):
+    chat_response = client_mistalai.chat(
+        model="open-mistral-7b",
+        messages=[ChatMessage(role="user", content='''Tu es un académicien français. Tourne cette phrase
+            en français : '''+prompt_image+''' en faisant en sorte que la phrase soit comme le sous-titre
+            en langue française d'une image qui représente cette phrase. La phrase en français doit absolument 
+            employer le mot '''+word+''' qui a la définition suivante : '''+definition+'''. ''')],
+        max_tokens=100
+    )
+    return chat_response.choices[0].message.content[:chat_response.choices[0].message.content.find('.')]
+
+# def prompt_image(word, definition):
+#     return "Génère une image pour ce mot : "+word+" qui a la définition suivante : "+definition
+
+### database functions
+def word_exists(word, language):
+    response = client_dynamodb.query(
+        TableName=table_name,
+        KeyConditionExpression='word = :word AND #dynobase_language = :language',
+        ExpressionAttributeValues={
+            ':word': {'S': word},
+            ':language': {'S': language},
+        },
+        ExpressionAttributeNames={
+            '#dynobase_language': 'language'
+        }
+    )
+    return len(get_word(word, language)['Items'])>0
+
+def get_word(word, language):
+    return client_dynamodb.query(
+        TableName=table_name,
+        KeyConditionExpression='word = :word AND #dynobase_language = :language',
+        ExpressionAttributeValues={
+            ':word': {'S': word},
+            ':language': {'S': language},
+        },
+        ExpressionAttributeNames={
+            '#dynobase_language': 'language'
+        }
+    )
+
+def add_item_to_db(word, definition, language, image_url, caption):
+    item = {
+        'word':{'S':word},
+        'definition':{'S':definition},
+        'language':{'S': language},
+        'image_url':{'S': image_url},
+        'caption':{'S': caption},
+    }
+    response = client_dynamodb.put_item(
+        TableName='definitions', 
+        Item=item
+    )
+
+
+# dynamodb = boto3.resource('dynamodb',aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"], 
+#     aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"], region_name=region_aws)
+# table = dynamodb.Table('definitions')
 
 if __name__ == '__main__':
     app.run(debug=True)
